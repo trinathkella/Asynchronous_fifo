cbd9ae17b0dd4172f2faf8e4b0288d73b20b6f34
trinathkella <kellatrinath@gmail.com>
2026-07-23 23:18:46 +0530

    wr_gen update

--- tb/sctb/async_fifo_wr_gen.sv
+++ tb/sctb/async_fifo_wr_gen.sv
@@ -15,12 +15,32 @@ class wr_gen;
 	mailbox g2d_mbox;
 	virtual inf v_if;
 
-	function new();
+	function new(virtual inf v_if);
+		this.v_if = v_if;
 		wr_t    = new();
 		g2d_mbox = new();
 	endfunction
 
-	task something;
+	task write_full_depth;
+		repeat(v_if.DEPTH)
+		begin
+			wr_t.txn_wr_en 	 = 1'b1;
+			wr_t.txn_wr_data = wr_t.txn_wr_data + 1;
+		end
 	endtask
+
+	task wrs_with_idle_cycles;
+		@(posedge v_if.wr_clk);
+		wr_t.txn_wr_data <= wr_t.txn_wr_data + 1;
+		repeat(2)(@posedge v_if.wr_clk);
+		wr_t.txn_wr_data <= wr_t.txn_wr_data
+	endtask
+
+	task ilegal_write;
+		if(v_if.full)
+			wr_t.txn_wr_data <= 32'hDEADBEEF;
+	endtask
+
+
 
 endclass : wr_gen
